752458f94369b539c5de0a70657c85624c7b9090
b3nik <jan.benik47@gmail.com>
2023-06-11 00:23:27 +0200

    button

--- src/AramidBOS.jsx
+++ src/AramidBOS.jsx
@@ -26,21 +26,61 @@ const Wrapper = styled.div`
     font-weight: bold;
     }
 
-    .alert{
-      text-align: center; 
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      flex-direction: column;
-      border: 1px solid red;
-      margin-left: auto;
-      margin-right: auto;
-      width: 50%;
+  .alert{
+    text-align: center; 
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    border: 1px solid red;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
     }
 
+  .btn {
+    align-items: center;
+    background-image: linear-gradient(144deg,#AF40FF, #5B42F3 50%,#00DDEB);
+    border: 0;
+    border-radius: 8px;
+    box-shadow: rgba(151, 65, 252, 0.2) 0 15px 30px -5px;
+    box-sizing: border-box;
+    color: #FFFFFF;
+    display: flex;
+    font-family: Phantomsans, sans-serif;
+    font-size: 20px;
+    justify-content: center;
+    line-height: 1em;
+    max-width: 100%;
+    min-width: 140px;
+    padding: 3px;
+    text-decoration: none;
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+    white-space: nowrap;
+    cursor: pointer;
+    } 
+    
+  .btn:active,
+  .btn:hover {
+    outline: 0;
+  }
+
+  .btn span {
+    background-color: rgb(5, 6, 45);
+    padding: 16px 24px;
+    border-radius: 6px;
+    width: 100%;
+    height: 100%;
+    transition: 300ms;
+  }
+
+  .btn:hover span {
+    background: none;
+  }
 
 `;
-
 
 initState({
   ipfsHash: null,
